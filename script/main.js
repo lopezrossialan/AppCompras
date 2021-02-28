f4dcd828675c73a3d25bb56b1b9e6be94c3b1009
lopezrossialan <lopezrossialan@hotmail.com>
2021-02-28 17:10:45 -0300

    app compras

---
+++ script/main.js
@@ -0,0 +1,38 @@
+var windowLoad = new bootstrap.Modal(document.getElementById('windowLoad'), {})
+let windowEmpty = document.getElementById('windowEmpty')
+let windowLoadList = document.getElementById('windowLoadList')
+let windowList = document.getElementById('windowList')
+let windowDetail = document.getElementById('windowDetail')
+
+let guardar = document.getElementById('windowLoadAdd').addEventListener('click', function() {
+    let producto = document.getElementById('windowLoadProduct').value
+    let icono = document.getElementById('windowLoadIcon').value
+    let info = document.getElementById('windowLoadInfo').value
+    document.getElementById('windowLoadProduct').value = ""
+    document.getElementById('windowLoadIcon').value = ""
+    document.getElementById('windowLoadInfo').value = ""
+    let modelo = ` <li class="list-group-item" data-producto="${producto}" data-icono="${icono}"  data-info="${info}"> <img src="${icono}" alt="${producto}" class="windowList__icon"> ${producto}</li> `
+    console.log("hola", producto, icono, info, windowLoadList)
+    windowLoadList.innerHTML += modelo
+    windowLoad.hide()
+    windowEmpty.style.display = 'none'
+    windowList.style.display = 'block'
+})
+
+let windowLoadListClick = document.getElementById('windowLoadList').addEventListener('click', function(e) {
+    document.getElementById('windowDetailProduct').innerHTML = e.target.getAttribute('data-producto')
+    document.getElementById('windowDetail__icon').src = e.target.getAttribute('data-icon')
+    document.getElementById('windowDetailInfo').innerHTML = e.target.getAttribute('data-info')
+    windowList.style.display = 'none'
+    windowDetail.style.display = 'block'
+
+})
+
+let windowDetaiLClose = document.getElementById('windowDetailClose').addEventListener('click', () => {
+    windowList.style.display = 'block'
+    windowDetail.style.display = 'none'
+
+})
+
+
+// let test = 0
